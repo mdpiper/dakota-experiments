3de80373af430289d7c606d3c56281d2e8f3ef30
Mark Piper <mark.piper@colorado.edu>
2014-11-19 11:48:06 -0700

    Add show and outfile arguments to plot_tabular_2d
    Also use the less-clumsy index notation for accessing hash values.

--- utils/dakota_utils.py
+++ utils/dakota_utils.py
@@ -35,14 +35,15 @@ def read_tabular(dat_file):
     return {'names':names, 'data':data}
 
 
-def plot_tabular_2d(tab_data, response_index=-1, title='Dakota Output'):
+def plot_tabular_2d(tab_data, response_index=-1, title='Dakota Output', 
+                    show=False, outfile='dakota_output.png'):
     '''
     Displays a surface plot of tabular output from a Dakota parameter
     study with two parameters and one or more response functions.
     '''
-    x = tab_data.get('data')[1,]
-    y = tab_data.get('data')[2,]
-    z = tab_data.get('data')[response_index,]
+    x = tab_data['data'][1,]
+    y = tab_data['data'][2,]
+    z = tab_data['data'][response_index,]
 
     m = len(set(x))
     n = len(set(y))
@@ -54,8 +55,14 @@ def plot_tabular_2d(tab_data, response_index=-1, title='Dakota Output'):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(X, Y, Z, rstride=1, cstride=1)
-    ax.set_xlabel(tab_data.get('names')[1])
-    ax.set_ylabel(tab_data.get('names')[2])
-    ax.set_zlabel(tab_data.get('names')[response_index])
+    ax.set_xlabel(tab_data['names'][1])
+    ax.set_ylabel(tab_data['names'][2])
+    ax.set_zlabel(tab_data['names'][response_index])
     plt.title(title)
-    plt.show(block=False)
+
+    # Either show plot or save it to a PNG file.
+    if show == True:
+        plt.show(block=False)
+    else:
+        plt.savefig(outfile, dpi=96)
+        plt.close()
