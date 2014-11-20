a64a494a291337b56cf2a86cc8c0987ccbca48f1
Mark Piper <mark.piper@colorado.edu>
2014-11-19 21:19:49 -0700

    Update setup.py
    The executable script produced by `install` is now working.

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 #! /usr/bin/env python
 
 from ez_setup import use_setuptools # https://pypi.python.org/pypi/setuptools
-
 use_setuptools()
-
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-
 from experiments import __version__
 
 setup(
-    name='foobar',
+    name='dakota_experiments',
     version=__version__,
     description='Experiments on CSDMS models with the DAKOTA iterative systems analysis toolkit',
-    url='http://csdms.colorado.edu',
+    url='https://github.com/mdpiper/dakota-experiments',
     author='Mark Piper',
     author_email='mark.piper@colorado.edu',
     license='MIT',
-    packages=find_packages(exclude='test*'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='CSDMS modeling DAKOTA',
+    packages=find_packages(),
     install_requires=['numpy', 'matplotlib'],
     entry_points={
         'console_scripts': [
