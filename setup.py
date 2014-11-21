2443acda76222153a910762a9c4cc53ddff232f4
Mark Piper <mark.piper@colorado.edu>
2014-11-21 14:33:45 -0700

    Update setup.py to exclude nosetests
    Also add long_description and platforms keywords.

--- setup.py
+++ setup.py
@@ -1,26 +1,46 @@
 #! /usr/bin/env python
+#
+# dakota-experiments
+#
+# Mark Piper (mark.piper@colorado.edu)
 
 from ez_setup import use_setuptools # https://pypi.python.org/pypi/setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 from experiments import __version__
 
+# Get the long description from the README file.
+def get_long_description():
+    from codecs import open
+    from os import path
+
+    here = path.abspath(path.dirname(__file__))
+    try:
+        with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+            long_description = f.read()
+    except:
+        return []
+    else:
+        return long_description
+
 setup(
-    name='dakota_experiments',
+    name='dakota-experiments',
     version=__version__,
-    description='Experiments on CSDMS models with the DAKOTA iterative systems analysis toolkit',
+    description='Systems analysis computer experiments on CSDMS models',
+    long_description=get_long_description(),
     url='https://github.com/mdpiper/dakota-experiments',
     author='Mark Piper',
     author_email='mark.piper@colorado.edu',
     license='MIT',
+    platforms=['any'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
     ],
-    keywords='CSDMS modeling DAKOTA',
-    packages=find_packages(),
+    keywords='CSDMS, earth systems modeling, DAKOTA, systems analysis',
+    packages=find_packages(exclude=['*.tests']),
     install_requires=['numpy', 'matplotlib'],
     entry_points={
         'console_scripts': [
