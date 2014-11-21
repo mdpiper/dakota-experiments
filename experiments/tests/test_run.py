9d9b4c187133aa659f18b2b3dd324cf4d3391c0c
Mark Piper <mark.piper@colorado.edu>
2014-11-21 14:35:47 -0700

    Add nosetests for run.py module

---
+++ experiments/tests/test_run.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+
+from nose.tools import *
+import os
+import tempfile
+import shutil
+from experiments.scripts.run import *
+
+def setup_module():
+    os.environ['_test_tmp_dir'] = tempfile.mkdtemp()
+
+def teardown_module():
+    shutil.rmtree(os.environ['_test_tmp_dir'])
+
+@raises(TypeError)
+def test_no_experiment():
+    run_experiment()
+
+def test_no_experiment_content():
+    err_str = 'Error: DAKOTA input file not found.'
+    r = run_experiment(os.environ['_test_tmp_dir'])
+    assert_equal(r, err_str)
+
+def test_no_input_file():
+    r = get_input_file(os.environ['_test_tmp_dir'])
+    assert_is_none(r)
