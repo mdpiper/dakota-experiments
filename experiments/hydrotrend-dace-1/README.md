d77a54cafed95644a8d5c6e6965792938dd8045c
Mark Piper <mark.piper@colorado.edu>
2014-12-05 13:45:53 -0700

    Start setting up HydroTrend DACE experiment

---
+++ experiments/hydrotrend-dace-1/README.md
@@ -0,0 +1,67 @@
+# HydroTrend Experiment 2
+
+In this computer experiment,
+a two-dimensional parameter study of HydroTrend
+is performed with Dakota.
+
+The default parameter values defined for HydroTrend in WMT,
+in the files
+<a href="./HYDRO_IN/HYDRO.IN">HYDRO.IN</a>
+and
+<a href="./HYDRO_IN/HYDRO0.HYPS">HYDRO0.HYPS</a>,
+are used as a starting point.
+Two HydroTrend parameters,
+_starting mean annual temperature_ (#8, here `T`, in Celsius) and
+_starting mean annual precipitation_ (#9, here `P`, in m),
+are perturbed
+to determine their effects on
+_long-term suspended sediment load_, `Qs`,
+over 36500 days of simulation time.
+
+Given lower and upper bounds on the values of `T` and `P`,
+the parameter space is partitioned into
+five equal intervals in each dimension,
+forming a grid.
+A HydroTrend simulation is performed
+for the parameter values at each node of the grid,
+for a total of 36 runs.
+For each run,
+the mean and standard deviation of the resulting `Qs` series
+are calculated.
+
+Run this experiment with:
+
+```bash
+$ dakota -i dakota-hydrotrend-2.in -o dakota-hydrotrend-2.out &> run.log
+```
+
+Instructions are given to Dakota through the input file
+<a href="./dakota-hydrotrend-2.in">dakota-hydrotrend-2.in</a>.
+Dakota calls the executable Python script
+<a href="./run_hydrotrend.py">run_hydrotrend.py</a>,
+which acts as a broker between Dakota and HydroTrend,
+with communication 
+through temporary files on the filesystem.
+Output from Dakota is captured in
+**dakota-hydrotrend-2.out**.
+Tabular results of the experiment
+are stored in
+**dakota-hydrotrend-2.dat**.
+Messages from stdout and stderr are filed in
+**run.log**.
+Clean up intermediate results with the shell script
+<a href="./dakota_cleanup">dakota_cleanup</a>.
+
+## Notes
+
+* Two response functions are used for the mean and standard deviation
+  of the `Qs` series.
+* The bounds on `T` and `P` were chosen to be
+  close to the default values used in WMT.
+* HydroTrend automatically updates the monthly mean climate variables
+  (#12-23), based on the values of `T` and `P`. However, the standard
+  deviations are not altered.
+* The minimum value of `P` must be greater than the value of _constant
+  annual base flow_, parameter #11.
+* HydroTrend gives warnings that there is snow remaining on Aug 31 for
+  `T` <= 12.
---
+++ experiments/hydrotrend-dace-1/run_hydrotrend.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+# Brokers communication between HydroTrend and Dakota through files.
+# Mark Piper (mark.piper@colorado.edu)
+
+import sys
+import os
+import re
+import shutil
+from subprocess import call
+import numpy as np
+
+
+def read(output_file):
+    '''
+    Reads a column of text containing HydroTrend output. Returns a numpy array.
+    '''
+    return np.loadtxt(output_file, skiprows=2)
+
+
+def write(results_file, array, labels=['Qs_mean', 'Qs_stdev']):
+    '''
+    Writes a Dakota results file from an input array.
+    '''
+    try:
+        fp = open(results_file, 'w')
+        for i in range(len(array)):
+            fp.write(str(array[i]) + '\t' + labels[i] + '\n')
+    except IOError:
+        raise
+    finally:
+        fp.close()
+
+
+def get_labels(params_file):
+    '''
+    Uses a regular expression to extract labels from a Dakota parameters file.
+    '''
+    labels = []
+    try:
+        fp = open(params_file, 'r')
+        for line in fp:
+            if re.search('ASV_', line):
+                labels.append(''.join(re.findall(':(\S+)', line)))
+    except IOError:
+        raise
+    finally:
+        fp.close()
+        return(labels)
+
+
+# Files and directories.
+start_dir = os.path.dirname(os.path.realpath(__file__))
+input_dir = os.path.join(start_dir, 'HYDRO_IN')
+if not os.path.exists(input_dir): os.mkdir(input_dir)
+output_dir = os.path.join(start_dir, 'HYDRO_OUTPUT')
+if not os.path.exists(output_dir): os.mkdir(output_dir)
+input_template = 'HYDRO.IN.template'
+input_file = 'HYDRO.IN'
+output_file = 'HYDROASCII.QS'
+
+# Use the parsing utility `dprepro` (from $DAKOTA_DIR/bin) to
+# incorporate the parameters from Dakota into the HydroTrend input
+# template, creating a new HydroTrend input file.
+shutil.copy(os.path.join(start_dir, input_template), os.curdir)
+call(['dprepro', sys.argv[1], input_template, input_file])
+shutil.copy(input_file, input_dir)
+
+# Call HydroTrend, using the updated input file.
+call(['hydrotrend', '--in-dir', input_dir, '--out-dir', output_dir])
+
+# Calculate mean and standard deviation of Qs for the simulation
+# time. Write the output to a Dakota results file.
+shutil.copy(os.path.join(output_dir, output_file), os.curdir)
+Qs = read(output_file)
+m_Qs = [np.mean(Qs), np.std(Qs)]
+labels = get_labels(sys.argv[1])
+write(sys.argv[2], m_Qs, labels)
