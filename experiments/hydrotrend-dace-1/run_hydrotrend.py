d22f5936180853f59ac64d29f1580f08fe938e1c
Mark Piper <mark.piper@colorado.edu>
2014-12-08 16:55:10 -0700

    Set up Dakota input file and Python broker script

--- experiments/hydrotrend-dace-1/run_hydrotrend.py
+++ experiments/hydrotrend-dace-1/run_hydrotrend.py
@@ -12,9 +12,17 @@ import numpy as np
 
 def read(output_file):
     '''
-    Reads a column of text containing HydroTrend output. Returns a numpy array.
+    Reads a column of text containing HydroTrend output. Returns a numpy array,
+    or None on an error.
     '''
-    return np.loadtxt(output_file, skiprows=2)
+    try:
+        a = np.loadtxt(output_file, skiprows=2)
+    except IOError:
+        raise
+    except StopIteration:
+        raise
+    finally:
+        return(a)
 
 
 def write(results_file, array, labels=['Qs_mean', 'Qs_stdev']):
@@ -48,30 +56,39 @@ def get_labels(params_file):
         return(labels)
 
 
-# Files and directories.
-start_dir = os.path.dirname(os.path.realpath(__file__))
-input_dir = os.path.join(start_dir, 'HYDRO_IN')
-if not os.path.exists(input_dir): os.mkdir(input_dir)
-output_dir = os.path.join(start_dir, 'HYDRO_OUTPUT')
-if not os.path.exists(output_dir): os.mkdir(output_dir)
-input_template = 'HYDRO.IN.template'
-input_file = 'HYDRO.IN'
-output_file = 'HYDROASCII.QS'
+def main():
 
-# Use the parsing utility `dprepro` (from $DAKOTA_DIR/bin) to
-# incorporate the parameters from Dakota into the HydroTrend input
-# template, creating a new HydroTrend input file.
-shutil.copy(os.path.join(start_dir, input_template), os.curdir)
-call(['dprepro', sys.argv[1], input_template, input_file])
-shutil.copy(input_file, input_dir)
+    # Files and directories.
+    start_dir = os.path.dirname(os.path.realpath(__file__))
+    input_dir = os.path.join(start_dir, 'HYDRO_IN')
+    if not os.path.exists(input_dir): os.mkdir(input_dir)
+    output_dir = os.path.join(start_dir, 'HYDRO_OUTPUT')
+    if not os.path.exists(output_dir): os.mkdir(output_dir)
+    input_template = 'HYDRO.IN.template'
+    input_file = 'HYDRO.IN'
+    output_file = 'HYDROASCII.QS'
 
-# Call HydroTrend, using the updated input file.
-call(['hydrotrend', '--in-dir', input_dir, '--out-dir', output_dir])
+    # Use the parsing utility `dprepro` (from $DAKOTA_DIR/bin) to
+    # incorporate the parameters from Dakota into the HydroTrend input
+    # template, creating a new HydroTrend input file.
+    shutil.copy(os.path.join(start_dir, input_template), os.curdir)
+    call(['dprepro', sys.argv[1], input_template, input_file])
+    shutil.copy(input_file, input_dir)
 
-# Calculate mean and standard deviation of Qs for the simulation
-# time. Write the output to a Dakota results file.
-shutil.copy(os.path.join(output_dir, output_file), os.curdir)
-Qs = read(output_file)
-m_Qs = [np.mean(Qs), np.std(Qs)]
-labels = get_labels(sys.argv[1])
-write(sys.argv[2], m_Qs, labels)
+    # Call HydroTrend, using the updated input file.
+    call(['hydrotrend', '--in-dir', input_dir, '--out-dir', output_dir])
+
+    # Calculate mean and standard deviation of a HydroTrend output time
+    # series for the simulation. Write the output to a Dakota results file.
+    shutil.copy(os.path.join(output_dir, output_file), os.curdir)
+    labels = get_labels(sys.argv[1])
+    series = read(output_file)
+    if series != None:
+        m_series = [np.mean(series), np.std(series)]
+    else:
+        m_series = [0, 0]
+    write(sys.argv[2], m_series, labels)
+
+
+if __name__ == '__main__':
+    main()
