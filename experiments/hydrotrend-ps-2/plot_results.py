26c8fc334f6f5c13fa1ad5e90f8c1793fabef53a
Mark Piper <mark.piper@colorado.edu>
2014-12-11 10:37:02 -0700

    Add plot_results script
    Makes a standard set of plots from Dakota output.

---
+++ experiments/hydrotrend-ps-2/plot_results.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Makes a standard set of plots from Dakota output.
+# Mark Piper (mark.piper@colorado.edu)
+
+# Note that these imports are from the installed version of dakota_utils.
+from dakota_utils.read import read_tabular
+from dakota_utils.plot import plot_samples, plot_irregular_surface
+
+tab_file = 'dakota-hydrotrend-2.dat'
+tab_data = read_tabular(tab_file)
+
+plot_surface(tab_data, response_index=-2, \
+             title='HydroTrend: Mean Qs(T,P)', \
+             outfile='dakota-hydrotrend-ps-2-Qs_mean-5x5.png')
+
+plot_surface(tab_data, response_index=-1, \
+             title='HydroTrend: Stdev Qs(T,P)', \
+             outfile='dakota-hydrotrend-ps-2-Qs_stdev-5x5.png')
