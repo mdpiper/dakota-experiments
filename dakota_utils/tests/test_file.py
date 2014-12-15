5f58546a6ff5b400919c744bf79a74e9d3969c8b
Mark Piper <mark.piper@colorado.edu>
2014-12-15 13:23:29 -0700

    Add more unit tests for file module

--- dakota_utils/tests/test_file.py
+++ dakota_utils/tests/test_file.py
@@ -48,3 +48,73 @@ def test_touch_zero_arguments():
     '''
     touch()
 
+def test_remove_file_does_not_exist():
+    '''
+    Tests deleting a nonexistent file with remove_file().
+    '''    
+    fname = 'vwbwguv00240cnwuncdsv'
+    remove_file(os.path.join(os.environ['_test_tmp_dir'], fname))
+
+def test_remove_directory_does_not_exist():
+    '''
+    Tests deleting a nonexistent directory with remove_directory().
+    '''    
+    dname = 'vwbwguv00240cnwuncdsv'
+    remove_directory(os.path.join(os.environ['_test_tmp_dir'], dname))
+
+def test_remove_does_not_exist():
+    '''
+    Tests deleting a nonexistent file or directory with remove().
+    '''    
+    name = 'vwbwguv00240cnwuncdsv'
+    remove(os.path.join(os.environ['_test_tmp_dir'], name))
+
+def test_remove_file():
+    '''
+    Tests that remove_file() deletes a file.
+    '''    
+    bname = 'delete_me'
+    fname = os.path.join(os.environ['_test_tmp_dir'], bname)
+    touch(fname)
+    remove_file(fname)
+    assert_false(os.path.exists(fname))
+
+def test_remove_directory():
+    '''
+    Tests that remove_directory() deletes a directory.
+    '''    
+    bname = 'delete_me'
+    dname = os.path.join(os.environ['_test_tmp_dir'], bname)
+    os.mkdir(dname)
+    remove_directory(dname)
+    assert_false(os.path.exists(dname))
+
+def test_remove_a_file():
+    '''
+    Tests that remove() deletes a file. (Uses touch)
+    '''
+    bname = 'delete_me'
+    fname = os.path.join(os.environ['_test_tmp_dir'], bname)
+    touch(fname)
+    remove(fname)
+    assert_false(os.path.exists(fname))
+
+def test_remove_a_directory():
+    '''
+    Tests that remove() deletes a directory.
+    '''    
+    bname = 'delete_me'
+    dname = os.path.join(os.environ['_test_tmp_dir'], bname)
+    os.mkdir(dname)
+    remove(dname)
+    assert_false(os.path.exists(dname))
+
+def test_touch():
+    '''
+    Tests that touch() makes a file. (Uses remove)
+    '''
+    bname = 'a_file'
+    fname = os.path.join(os.environ['_test_tmp_dir'], bname)
+    touch(fname)
+    assert_true(os.path.exists(fname))
+    remove(fname)
