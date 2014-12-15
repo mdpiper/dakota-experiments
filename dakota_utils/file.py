3aa6c41306711639b7ce2076a2f54b9eeee8fefd
Mark Piper <mark.piper@colorado.edu>
2014-12-15 11:48:14 -0700

    Add remove_file() and remove_directory() functions

--- dakota_utils/file.py
+++ dakota_utils/file.py
@@ -5,15 +5,39 @@
 # Mark Piper (mark.piper@colorado.edu)
 
 import os
+import shutil
 from subprocess import check_call
 
 
-def remove(fname):
+def remove(target):
+    '''
+    Deletes a file or directory. Silently passes if the file or directory
+    doesn't exist.
+    '''
+    try:
+        remove_file(target)
+        remove_directory(target)
+    except OSError:
+        pass
+
+
+def remove_file(fname):
     '''
     Deletes a file. Silently passes if the file doesn't exist.
     '''
     try:
         os.remove(fname)
+    except OSError:
+        pass
+
+
+def remove_directory(dname):
+    '''
+    Recursively deletes a directory. Silently passes if the directory
+    doesn't exist.
+    '''
+    try:
+        shutil.rmtree(dname)
     except OSError:
         pass
 
