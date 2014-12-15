6c5e7b18aa70cb01637e8590a0b330e689a51793
Mark Piper <mark.piper@colorado.edu>
2014-12-15 16:45:38 -0700

    Add strip_interface_cloumn() to write module

--- dakota_utils/write.py
+++ dakota_utils/write.py
@@ -4,6 +4,9 @@
 #
 # Mark Piper (mark.piper@colorado.edu)
 
+import shutil
+from subprocess import check_call, CalledProcessError
+
 def write_results(results_file, array, labels):
     '''
     Writes a Dakota results file from an input array.
@@ -16,3 +19,16 @@ def write_results(results_file, array, labels):
         raise
     finally:
         fp.close()
+
+
+def strip_interface_column(tab_file):
+    '''
+    Strips the 'interface' column from a Dakota 6.1 tabular output file.
+    '''
+    try:
+        bak_file = tab_file + '.orig'
+        shutil.copyfile(tab_file, bak_file)
+        cmd = 'cat ' + bak_file +' | colrm 9 18 > ' + tab_file
+        check_call(cmd, shell=True)
+    except (IOError, CalledProcessError):
+        raise
