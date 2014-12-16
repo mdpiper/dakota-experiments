b2976c66b33cccbaff79aeeee76d32aa536fb6b3
Mark Piper <mark.piper@colorado.edu>
2014-12-16 13:46:29 -0700

    Reduce number of arguments in grid_lhs()
    Makes the calling syntax slightly cleaner.

--- dakota_utils/grid.py
+++ dakota_utils/grid.py
@@ -7,7 +7,7 @@
 import numpy as np
 from scipy.interpolate import griddata
 
-def grid_lhs(tab_data, nx, ny, boundsx, boundsy, response_index=-1):
+def grid_lhs(tab_data, nxy, boundsx, boundsy, response_index=-1):
     '''
     Interpolates a response function defined at a set of irregular 
     LHS locations to a regular grid.
@@ -17,8 +17,8 @@ def grid_lhs(tab_data, nx, ny, boundsx, boundsy, response_index=-1):
     z = tab_data['data'][response_index,]
     
     # np.mgrid is tricky.
-    grid_x, grid_y = np.mgrid[boundsx[0]:boundsx[1]:complex(nx), \
-                              boundsy[0]:boundsy[1]:complex(ny)]
+    grid_x, grid_y = np.mgrid[boundsx[0]:boundsx[1]:complex(nxy[0]), \
+                              boundsy[0]:boundsy[1]:complex(nxy[1])]
     grid_z = griddata(xy_t, z, (grid_x, grid_y), method='cubic')
 
     return(grid_z, grid_x, grid_y)
--- dakota_utils/plot.py
+++ dakota_utils/plot.py
@@ -76,7 +76,7 @@ def plot_irregular_surface(tab_data, response_index=-1,
     from .grid import grid_lhs
 
     # TODO: Educe these values from dakota.in.
-    gz, gx, gy = grid_lhs(tab_data, 40, 30, [-10,30], [0.1,6.1], response_index)
+    gz, gx, gy = grid_lhs(tab_data, (40,30), (-10,30), (0.1,6.1), response_index)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
