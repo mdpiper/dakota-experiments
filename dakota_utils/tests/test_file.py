4aca30e376b2310e2436fdb799bf3cae1c9a1d2b
Mark Piper <mark.piper@colorado.edu>
2014-12-15 17:02:37 -0700

    Define global variables to clean up tests

--- dakota_utils/tests/test_file.py
+++ dakota_utils/tests/test_file.py
@@ -13,6 +13,10 @@ import tempfile
 import shutil
 from dakota_utils.file import *
 
+nondir = 'vwbwguv00240cnwuncdsv'
+nonfile = nondir + '.pro'
+bname = 'delete_me'
+
 def setup_module():
     print('File tests:')
     os.environ['_test_tmp_dir'] = tempfile.mkdtemp()
@@ -52,28 +56,25 @@ def test_remove_file_does_not_exist():
     '''
     Tests deleting a nonexistent file with remove_file().
     '''    
-    fname = 'vwbwguv00240cnwuncdsv'
-    remove_file(os.path.join(os.environ['_test_tmp_dir'], fname))
+    remove_file(os.path.join(os.environ['_test_tmp_dir'], nonfile))
 
 def test_remove_directory_does_not_exist():
     '''
     Tests deleting a nonexistent directory with remove_directory().
     '''    
-    dname = 'vwbwguv00240cnwuncdsv'
-    remove_directory(os.path.join(os.environ['_test_tmp_dir'], dname))
+    remove_directory(os.path.join(os.environ['_test_tmp_dir'], nondir))
 
 def test_remove_does_not_exist():
     '''
     Tests deleting a nonexistent file or directory with remove().
     '''    
-    name = 'vwbwguv00240cnwuncdsv'
-    remove(os.path.join(os.environ['_test_tmp_dir'], name))
+    remove(os.path.join(os.environ['_test_tmp_dir'], nonfile))
+    remove(os.path.join(os.environ['_test_tmp_dir'], nondir))
 
 def test_remove_file():
     '''
     Tests that remove_file() deletes a file.
     '''    
-    bname = 'delete_me'
     fname = os.path.join(os.environ['_test_tmp_dir'], bname)
     touch(fname)
     remove_file(fname)
@@ -83,7 +84,6 @@ def test_remove_directory():
     '''
     Tests that remove_directory() deletes a directory.
     '''    
-    bname = 'delete_me'
     dname = os.path.join(os.environ['_test_tmp_dir'], bname)
     os.mkdir(dname)
     remove_directory(dname)
@@ -93,7 +93,6 @@ def test_remove_a_file():
     '''
     Tests that remove() deletes a file. (Uses touch)
     '''
-    bname = 'delete_me'
     fname = os.path.join(os.environ['_test_tmp_dir'], bname)
     touch(fname)
     remove(fname)
@@ -103,7 +102,6 @@ def test_remove_a_directory():
     '''
     Tests that remove() deletes a directory.
     '''    
-    bname = 'delete_me'
     dname = os.path.join(os.environ['_test_tmp_dir'], bname)
     os.mkdir(dname)
     remove(dname)
@@ -113,8 +111,7 @@ def test_touch():
     '''
     Tests that touch() makes a file. (Uses remove)
     '''
-    bname = 'a_file'
-    fname = os.path.join(os.environ['_test_tmp_dir'], bname)
+    fname = os.path.join(os.environ['_test_tmp_dir'], 'a_file')
     touch(fname)
     assert_true(os.path.exists(fname))
     remove(fname)
