4921d58775faa65423fac321ef68f065b2499813
Mark Piper <mark.piper@colorado.edu>
2014-12-18 10:25:57 -0700

    Update script for Dakota 6.1 tabular output file

--- experiments/hydrotrend-uq-1/plot_results.py
+++ experiments/hydrotrend-uq-1/plot_results.py
@@ -5,17 +5,20 @@
 # Note that these imports are from the installed version of dakota_utils.
 from dakota_utils.read import read_tabular
 from dakota_utils.plot import plot_samples, plot_irregular_surface
+from dakota_utils.convert import has_interface_column, strip_interface_column
 
 tab_file = 'dakota.dat'
+if has_interface_column(tab_file):
+    strip_interface_column(tab_file)
 tab_data = read_tabular(tab_file)
 
 plot_samples(tab_data, \
-             outfile='dakota-hydrotrend-dace-1-lhs-samples.png')
+             outfile='dakota-hydrotrend-uq-1-lhs-samples.png')
 
 plot_irregular_surface(tab_data, response_index=-2, \
-                       title='HydroTrend: Mean Qs(T,P)', \
-                       outfile='dakota-hydrotrend-dace-1-Qs_mean.png')
+                       title='HydroTrend: Mean Q(T,P)', \
+                       outfile='dakota-hydrotrend-uq-1-Q_mean.png')
 
 plot_irregular_surface(tab_data, response_index=-1, \
-                       title='HydroTrend: Stdev Qs(T,P)', \
-                       outfile='dakota-hydrotrend-dace-1-Qs_stdev.png')
+                       title='HydroTrend: Stdev Q(T,P)', \
+                       outfile='dakota-hydrotrend-uq-1-Q_stdev.png')
