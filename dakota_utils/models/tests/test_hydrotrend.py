510bcab40b6dd6adedc62ba8a3ea1f53b82dbc3c
Mark Piper <mark.piper@colorado.edu>
2014-12-17 17:28:54 -0700

    Start adding unit tests for HydroTrend class
    There will be many.

--- dakota_utils/models/tests/test_hydrotrend.py
+++ dakota_utils/models/tests/test_hydrotrend.py
@@ -11,16 +11,44 @@ from nose.tools import *
 import os
 import tempfile
 import shutil
-from dakota_utils.models.hydrotrend import *
+from dakota_utils.models.hydrotrend import HydroTrend
 
 nonfile = 'wvwbfb9vwvfnv.f'
 
 def setup_module():
     print('HydroTrend tests:')
-    os.environ['_test_tmp_dir'] = tempfile.mkdtemp()
+    os.environ['_test_hydrotrend_dir'] = tempfile.mkdtemp()
 
 def teardown_module():
-    shutil.rmtree(os.environ['_test_tmp_dir'])
+    shutil.rmtree(os.environ['_test_hydrotrend_dir'])
+
+def test_HydroTrend_no_arguments():
+    '''
+    Tests no arguments creates input and output directories.
+    '''
+    os.chdir(os.environ['_test_hydrotrend_dir'])
+    h = HydroTrend()
+    assert_true(os.path.exists(h.input_dir))
+    assert_true(os.path.exists(h.output_dir))
+
+def test_HydroTrend_set_input_dir():
+    '''
+    Tests setting the input directory on init.
+    '''
+    os.chdir(os.environ['_test_hydrotrend_dir'])
+    input_dir = '__hydro_in'
+    h = HydroTrend(input_dir)
+    assert_equal(h.input_dir, input_dir)
+
+def test_HydroTrend_set_output_dir():
+    '''
+    Tests setting the output directory on init.
+    '''
+    os.chdir(os.environ['_test_hydrotrend_dir'])
+    output_dir = '__hydro_out'
+    h = HydroTrend(None, output_dir)
+    assert_equal(h.output_dir, output_dir)
+
 
 # @raises(TypeError)
 # def test_load_no_arguments():
