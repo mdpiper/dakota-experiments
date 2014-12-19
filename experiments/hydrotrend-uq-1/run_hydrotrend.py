740b00093c6fbc2fca67e8f569f9cfc0ee4e3788
Mark Piper <mark.piper@colorado.edu>
2014-12-19 14:10:00 -0700

    Pass output_files and response_statistic to HydroTrend()

--- experiments/hydrotrend-uq-1/run_hydrotrend.py
+++ experiments/hydrotrend-uq-1/run_hydrotrend.py
@@ -10,20 +10,23 @@ from dakota_utils.models import hydrotrend
 def main():
     '''
     Sets up HydroTrend input, runs model, gathers output. 
-    '''    
-    if hydrotrend.is_installed() is False:
+    '''
+    if hydrotrend.is_installed():
+        h = hydrotrend.HydroTrend()
+    else:
         print('Error: HydroTrend executable cannot be located.')
         return
+
+    # The files and statistic used in the Dakota response.
+    h.output_files = ('HYDROASCII.Q', 'HYDROASCII.QS', 'HYDROASCII.QB')
+    h.response_statistic = 'median'
 
     # References to files passed by Dakota.
     params_file = sys.argv[1]
     results_file = sys.argv[2]
 
-    h = hydrotrend.HydroTrend()
-    h.output_files = ('HYDROASCII.Q', 'HYDROASCII.QS', 'HYDROASCII.QB')
-
     # Set up the HydroTrend run, taking information from the parameters
-    # file passed by Dakota.
+    # file created by Dakota.
     start_dir = os.path.dirname(os.path.realpath(__file__))
     h.setup(start_dir, params_file)
 
