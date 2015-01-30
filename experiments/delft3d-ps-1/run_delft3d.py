63bdb36753d0b3093d1d7389fa8adbf15aee9867
Mark Piper <mark.piper@colorado.edu>
2015-01-30 13:17:25 -0700

    Use a separate work directory for each Delft3D run
    Each run uses about 600 MB, so disable the 'directory_save' keyword
    after testing.

--- experiments/delft3d-ps-1/run_delft3d.py
+++ experiments/delft3d-ps-1/run_delft3d.py
@@ -3,22 +3,35 @@
 # Mark Piper (mark.piper@colorado.edu)
 
 import sys
+import os
 import shutil
 from subprocess import call
 
 
 def main():
     '''
-    Brokers communication between Delft3D and Dakota through files.
+    Brokers communication between Delft3D, MATLAB and Dakota through files.
     '''
 
     # Files and directories.
+    start_dir = os.path.dirname(os.path.realpath(__file__))
+    initialize_dir = os.path.join(start_dir, 'initialize')
     input_template = 'WLD.sed.template'
     input_file = 'WLD.sed'
+    cells_file = 'nesting.txt'
+    analysis_program_file = 'total_sed_cal.m'
+    analysis_results_file = 'results.out'
+
+    # Copy the contents of the initialize directory into the current
+    # run directory. (Don't use shutil.copytree because the
+    # destination directory exists.)
+    for f in os.listdir(initialize_dir):
+        shutil.copy(os.path.join(initialize_dir, f), os.getcwd())
 
     # Use the parsing utility `dprepro` (from $DAKOTA_DIR/bin) to
     # substitute the parameters from Dakota into the input template,
     # creating a new Delft3D input file.
+    shutil.copy(os.path.join(start_dir, input_template), os.getcwd())
     call(['dprepro', sys.argv[1], input_template, input_file])
 
     # Call Delft3D, using the updated input file.
@@ -26,9 +39,11 @@ def main():
 
     # Call a MATLAB script to read the Delft3D output, calculate the
     # desired responses, and write the Dakota results file.
-    call(['matlab', '-nojvm', '-nodisplay', '-nosplash', '-r', \
-          '"total_sed_cal; exit"'])
-    shutil.move('results.out', sys.argv[2])
+    shutil.copy(os.path.join(start_dir, cells_file), os.getcwd())
+    shutil.copy(os.path.join(start_dir, analysis_program_file), os.getcwd())
+    matlab_call = '"' + os.path.splitext(analysis_program_file)[0] + '; exit"'
+    call(['matlab', '-nojvm', '-nodisplay', '-nosplash', '-r', matlab_call])
+    # shutil.move(analysis_results_file, sys.argv[2])
 
 if __name__ == '__main__':
     main()
