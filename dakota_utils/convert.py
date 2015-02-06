08464422688bf0ecdf7d9902eafbd3f5f8c7d8f6
Mark Piper <mark.piper@colorado.edu>
2015-02-06 09:16:48 -0700

    Don't remove column 9 from tabular_data_file
    Doing so doesn't leave a space between columns 1 and 3.

--- dakota_utils/convert.py
+++ dakota_utils/convert.py
@@ -30,7 +30,7 @@ def strip_interface_column(tab_file):
     try:
         bak_file = tab_file + '.orig'
         shutil.copyfile(tab_file, bak_file)
-        cmd = 'cat ' + bak_file +' | colrm 9 18 > ' + tab_file
+        cmd = 'cat ' + bak_file +' | colrm 10 18 > ' + tab_file
         check_call(cmd, shell=True)
     except (IOError, CalledProcessError):
         raise
