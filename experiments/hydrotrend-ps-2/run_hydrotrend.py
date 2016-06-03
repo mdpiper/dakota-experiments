17ea42d0eb8ce7c079a0f62d0f527ea39f66d6f6
Mark Piper <mark.piper@colorado.edu>
2016-06-03 15:59:18 -0600

    Work around HydroTrend file length issue
    HydroTrend exits with 'Abort Trap 6' if its `--in-dir` or
    `--out-dir` is more than 80 characters. Dakota knows where its
    current directly resides on the file system, so I think I can
    safely use relative paths -- `../HYDRO_IN` and `../HYDRO_OUTPUT`.

--- experiments/hydrotrend-ps-2/run_hydrotrend.py
+++ experiments/hydrotrend-ps-2/run_hydrotrend.py
@@ -76,7 +76,9 @@ def main():
     shutil.copy(input_file, input_dir)
 
     # Call HydroTrend, using the updated input file.
-    call(['hydrotrend', '--in-dir', input_dir, '--out-dir', output_dir])
+    call(['hydrotrend',
+          '--in-dir', os.path.relpath(input_dir),
+          '--out-dir', os.path.relpath(output_dir)])
 
     # Calculate mean and standard deviation of a HydroTrend output time
     # series for the simulation. Write the output to a Dakota results file.
